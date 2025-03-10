88207cd76c8e300627a4ceb439e6bc22835db886
Siri2512 <balabhadrasaisiri@gmail.com>
2025-03-10 18:00:14 +0530

    pes2ug22cs124pipeline

---
+++ main.cpp
@@ -0,0 +1,11 @@
+//File: hello.cpp
+#include <iostream>
+using namespace std;
+
+int main(void) {
+  cout << "PES2UG22CS124\n";
+  cout << "Hello, Jenkins\n";
+  cout << "Hello, PES2UG22CS124s\n";
+  cout << "MAIN.CPP\n";
+  return 0;
+}
